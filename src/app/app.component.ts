d413c741fe321b5cdc2e72144b77082b281f6e22
Muhammad Hilman Bin Ahmad <hilmanmanang@gmail.com>
2021-05-08 18:21:18 +0800

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  number: number = 12
-  isVisible: boolean = true;
-  user = {
-    name: "Hilman"
-  }
 
-  increment() {
-    this.number++
-  }
-
-  decrement() {
-    this.number--
-  }
-
-  update() {
-    this.user.name = "Abu";
-  }
-
-  visible() {
-    this.isVisible = !this.isVisible
-  }
 }
